37e3dedf4e64adcabfd7d08d37b38f1bf90604a8
altf4m88 <fadhilmauladhani@gmail.com>
2022-02-14 09:16:53 +0700

    auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
+            $table->uuid('userable_id');
+            $table->boolean('status');
             $table->rememberToken();
             $table->timestamps();
         });
