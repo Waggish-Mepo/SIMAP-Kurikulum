00b3c0ac44e0bed33a33a3ff79eb0f11a1bcdd3e
altf4m88 <fadhilmauladhani@gmail.com>
2022-02-17 09:40:55 +0700

    create reportp period service

---
+++ app/Service/Database/ReportPeriodService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Service\Database;
+
+use App\Models\Subject;
+use App\Models\ReportPeriod;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Ramsey\Uuid\Uuid;
+
+class ReportPeriodService{
+    public function index($filter = []) {
+
+        $orderBy = $filter['order_by'] ?? 'ASC';
+        $perPage = $filter['page'] ?? 20;
+        $schoolYear = $filter['school_year'] ?? null;
+
+        $query = ReportPeriod::orderBy('order', $orderBy);
+
+        if ($schoolYear) {
+            $query->where('school_year', $schoolYear);
+        }
+
+        $subjects = $query->paginate($perPage);
+
+        return $subjects;
+    }
+
+    public function detail($reportPeriodId) {
+
+        $subject = ReportPeriod::findOrFail($reportPeriodId);
+
+        return $subject->toArray();
+    }
+
+    public function create($payload) {
+
+        $reportPeriod = new ReportPeriod;
+
+        $reportPeriod->id = Uuid::uuid4()->toString();
+        $reportPeriod = $this->fill($reportPeriod, $payload);
+        $reportPeriod->save();
+
+        return $reportPeriod->toArray();
+    }
+
+    public function update($reportPeriodId, $payload) {
+
+        $reportPeriod = ReportPeriod::findOrFail($reportPeriodId);
+
+        $reportPeriod = $this->fill($reportPeriod, $payload);
+        $reportPeriod->save();
+
+        return $reportPeriod->toArray();
+    }
+
+
+    private function fill(ReportPeriod $reportPeriod, array $payload) {
+        foreach ($payload as $key => $value) {
+            $reportPeriod->$key = $value;
+        }
+
+        $validate = Validator::make($reportPeriod->toArray(), [
+            'title' => 'required|string',
+            'school_year' => ['required', Rule::in(config('constant.common.school_years'))],
+            'start_date' => 'nullable',
+            'end_date' => 'nullable',
+        ]);
+
+        if($validate->fails()) {
+            return $validate->errors()->toArray();
+        }
+
+        return $reportPeriod;
+    }
+
+}
