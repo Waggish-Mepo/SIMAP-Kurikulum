5aa4581adf496ab6940e6f43353cb81959ecfe75
altf4m88 <fadhilmauladhani@gmail.com>
2022-02-19 10:49:23 +0700

    update factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use App\Models\Subject;
 use App\Models\Teacher;
 use App\Models\User;
@@ -17,17 +18,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user = [
-            'name' => 'Admin Kurikulum',
-            'username' => 'KurikulumWikrama',
-            'password' => Hash::make('Wikrama2022'),
-            'role' => User::ADMIN,
-            'status' => true,
-            'userable_id' => Teacher::factory(['name' => 'Admin Kurikulum'])->create()->id,
+        $users = [
+            [
+                'name' => 'Admin Kurikulum',
+                'username' => 'KurikulumWikrama',
+                'password' => Hash::make('Wikrama2022'),
+                'role' => User::ADMIN,
+                'status' => true,
+                'userable_id' => Teacher::factory(['name' => 'Admin Kurikulum'])->create()->id,
+            ],
+            [
+                'name' => 'Siswa 1',
+                'username' => 'Siswa1',
+                'password' => Hash::make('Wikrama2022'),
+                'role' => User::STUDENT,
+                'status' => true,
+                'userable_id' => Student::factory(['name' => 'Siswa 1'])->create()->id,
+            ],
+            [
+                'name' => 'Siswa 2',
+                'username' => 'Siswa2',
+                'password' => Hash::make('Wikrama2022'),
+                'role' => User::STUDENT,
+                'status' => true,
+                'userable_id' => Student::factory(['name' => 'Siswa 2'])->create()->id,
+            ],
+            [
+                'name' => 'Siswa 3',
+                'username' => 'Siswa3',
+                'password' => Hash::make('Wikrama2022'),
+                'role' => User::STUDENT,
+                'status' => true,
+                'userable_id' => Student::factory(['name' => 'Siswa 3'])->create()->id,
+            ],
+            [
+                'name' => 'Guru 1',
+                'username' => 'Guru1',
+                'password' => Hash::make('Wikrama2022'),
+                'role' => User::TEACHER,
+                'status' => true,
+                'userable_id' => Teacher::factory(['name' => 'Guru 1'])->create()->id,
+            ],
+            [
+                'name' => 'Guru 2',
+                'username' => 'Guru2',
+                'password' => Hash::make('Wikrama2022'),
+                'role' => User::TEACHER,
+                'status' => true,
+                'userable_id' => Teacher::factory(['name' => 'Guru 2'])->create()->id,
+            ],
         ];
 
-        User::factory($user)->create();
+        foreach ($users as $user) {
+            User::factory($user)->create();
+        }
 
-        Subject::factory(['name' => 'Bahasa Indonesia', 'group' => 'A (Muatan Nasional)'])->create();
+        Subject::factory(['name' => 'Bahasa Indonesia', 'group' => Subject::MUATAN_A])->create();
+        Subject::factory(['name' => 'Bahasa Sunda', 'group' => Subject::MUATAN_B])->create();
+        Subject::factory(['name' => 'Pemrograman Dasar', 'group' => Subject::MUATAN_C])->create();
     }
 }
