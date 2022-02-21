7faea26471dceecb2f2fdf2665e7f22dc3a6b18d
Mochamad-Haikal <mhaikal1006@gmail.com>
2022-02-21 15:33:27 +0700

    migration batch

---
+++ database/migrations/2022_02_21_035416_create_batches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('major_id');
+            $table->string('entry_year')->nullable();
+            $table->string('bacth_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('batches');
+    }
+}
