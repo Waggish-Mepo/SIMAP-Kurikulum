20c8fff68443c62d14ac779c1dad48701fa2e37e
altf4m88 <fadhilmauladhani@gmail.com>
2022-03-06 11:46:56 +0700

    add scorecard component update & scorecard index

--- app/Models/Scorecard.php
+++ app/Models/Scorecard.php
@@ -10,4 +10,16 @@ class Scorecard extends Model
     use HasFactory;
 
     public $incrementing = false;
+
+    public function student() {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function gradebook() {
+        return $this->belongsTo(Gradebook::class);
+    }
+
+    public function scorecardComponents() {
+        return $this->hasMany(ScorecardComponent::class);
+    }
 }
