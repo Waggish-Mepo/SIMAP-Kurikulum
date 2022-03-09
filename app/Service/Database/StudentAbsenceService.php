c253941c1cf1f5e7cee3cfa68f89134ebf0dd4c6
Mochamad-Haikal <mhaikal1006@gmail.com>
2022-03-09 13:13:59 +0700

    Migration-Database-Service-Student-Absence-282ef9ec08c64b95b8be5c9cb569ec14

---
+++ app/Service/Database/StudentAbsenceService.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Service\Database;
+
+use App\Models\StudentAbsence;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Ramsey\Uuid\Uuid;
+
+class StudentAbsenceService{
+    public function index($filter = []) {
+
+        $orderBy = $filter['order_by'] ?? 'ASC';
+        $studentId = $filter['student_id'] ?? null;
+        $reportPeriodId = $filter['report_period_id'] ?? null;
+        $alpa = $filter['alpa'] ?? null;
+        $izin = $filter['izin'] ?? null;
+        $sakit = $filter['sakit'] ?? null;
+        $withoutPagination = $filter['without_pagination'] ?? false;
+
+        $query = StudentAbsence::orderBy('created_at', $orderBy);
+
+        if ($studentId) {
+            $query->where('student_id', $studentId);
+        }
+
+        if ($reportPeriodId) {
+            $query->where('report_period_id', $reportPeriodId);
+        }
+
+        if ($alpa) {
+            $query->where('alpa', $alpa);
+        }
+
+        if ($izin) {
+            $query->where('izin', $izin);
+        }
+
+        if ($sakit) {
+            $query->where('sakit', $sakit);
+        }
+
+        if ($withoutPagination) {
+            return $query->get()->toArray();
+        }
+    }
+
+    public function detail($studentAbsenceId) {
+
+        $absence = StudentAbsence::findOrFail($studentAbsenceId);
+
+        return $absence->toArray();
+    }
+
+    public function create($payload) {
+
+        $studentAbsence = new StudentAbsence;
+
+        $studentAbsence->id = Uuid::uuid4()->toString();
+        $studentAbsence = $this->fill($studentAbsence, $payload);
+        $studentAbsence->save();
+
+        return $studentAbsence->toArray();
+    }
+
+    public function update($studentAbsenceId, $payload) {
+
+        $studentAbsence = StudentAbsence::findOrFail($studentAbsenceId);
+
+        $studentAbsence = $this->fill($studentAbsence, $payload);
+        $studentAbsence->save();
+
+        return $studentAbsence->toArray();
+    }
+
+
+    private function fill(StudentAbsence $studentAbsence, array $payload) {
+        foreach ($payload as $key => $value) {
+            $studentAbsence->$key = $value;
+        }
+
+        $validate = Validator::make($studentAbsence->toArray(), [
+            'student_id' => 'required|uuid',
+            'report_period_id' => 'required|uuid',
+            'alpa' => 'required|numeric',
+            'izin' => 'required|numeric',
+            'sakit' => 'required|numeric',
+        ]);
+
+        if($validate->fails()) {
+            return $validate->errors()->toArray();
+        }
+
+        return $studentAbsence;
+    }
+
+}
