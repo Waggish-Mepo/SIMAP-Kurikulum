e523920ce9bc46c1ad6793da8a796384792fe3d9
Mochamad-Haikal <mhaikal1006@gmail.com>
2022-03-29 23:45:08 +0700

    migration attitude predicate

---
+++ app/Models/AttitudePredicate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Attitude;
+use App\Models\StudesntAttitude;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttitudePredicate extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    public function studentAttitudes() {
+        return $this->hasMany(StudentAttitude::class);
+    }
+
+    public function attitude() {
+        return $this->belongsTo(Attitude::class);
+    }
+}
