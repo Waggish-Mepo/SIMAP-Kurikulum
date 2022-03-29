92e78a96f148a7b7325ce2ed349daaba2d681c14
Mochamad-Haikal <mhaikal1006@gmail.com>
2022-03-30 00:04:10 +0700

    migration student attributes

---
+++ database/migrations/2022_03_29_164923_create_student_attributes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_attributes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('description');
+            $table->uuid('attitude_predicate_id');
+            $table->uuid('student_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_attributes');
+    }
+}
