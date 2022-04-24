a554f8f387b626c32ed3062207f974a343fa12f2
altf4m88 <fadhilmauladhani@gmail.com>
2022-04-25 04:20:54 +0700

    change length

---
+++ database/migrations/2022_04_24_121153_update_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->bigInteger('nisn')->change();
+            $table->bigInteger('nis')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->integer('nisn')->change();
+            $table->integer('nis')->change();
+        });
+    }
+}
